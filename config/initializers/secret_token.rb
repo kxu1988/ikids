15acccac4f8e5edcada0441f31e97037e975b2cd
yuping1987626 <yuping_1987@126.com>
2013-07-12 11:06:43 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IkidCygps::Application.config.secret_token = '3585082fefa74fdf5869dc4b09086027c784a48c909a8ebb9c311f7f1be4317b2500650c33d07a505296c2b9972ae9088909a9098c65e28731506f51d0b59c7a'
